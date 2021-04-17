49f22c75ae0b67bf49dc0e992dd6d58113c33ce9
aibees <aibees1129@naver.com>
2021-04-18 00:09:01 +0900

    [ADD] UserMasterVo

---
+++ server/apiServer/src/main/java/com/plaidcamp/mealogram/apiServer/user/vo/UserMaster.java
@@ -0,0 +1,40 @@
+package com.plaidcamp.mealogram.apiServer.user.vo;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name="USER_MASTER")
+public class UserMaster {
+
+
+    private String ID;
+
+    private int seq;
+
+    private String EMAIL;
+
+    private String password;
+
+    private int account_cnt;
+
+    private int pwerrcnt;
+
+    private String pwinitcode;
+
+    private String phone;
+
+    private String userclass;
+
+    private String facebook_key;
+
+    private String google_key;
+
+    private String kakao_key;
+
+    private LocalDateTime ctime;
+
+    private LocalDateTime utime;
+}
+
