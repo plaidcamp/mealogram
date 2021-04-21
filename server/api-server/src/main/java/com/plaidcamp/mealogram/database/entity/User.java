4b4e133988d9ee278e7812fd937a980e2b3c1292
Hyowon An <503109@naver.com>
2021-04-21 20:55:55 +0900

    Update User.java
    비밀번호 암호화 코드

--- server/api-server/src/main/java/com/plaidcamp/mealogram/database/entity/User.java
+++ server/api-server/src/main/java/com/plaidcamp/mealogram/database/entity/User.java
@@ -6,7 +6,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.PrePersist;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.hibernate.annotations.GenericGenerator;
 
 import lombok.NonNull;
@@ -27,5 +31,21 @@ public class User extends BaseTimeEntity {
     @Column(unique = true)
     @NonNull
     private String email;
+
+    @JsonIgnore
+    @Column(nullable = false)
+    private String password;
+
+    @PrePersist
+    void hashPassword() {
+        if (password == null || password.isEmpty()) {
+            throw new DataIntegrityViolationException("Empty password");
+        }
+        password = new BCryptPasswordEncoder().encode(this.password);
+    }
+
+    public boolean comparePassword(String password) {
+        return new BCryptPasswordEncoder().matches(password, this.password);
+    }
 
 }
