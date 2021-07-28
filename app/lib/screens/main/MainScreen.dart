47ed08c66537591560184f753e3ce5dfe91ccc80
JiHoon <wlgns5721@gmail.com>
2021-07-28 20:30:16 +0900

    토큰 처리 로직 수정

--- app/lib/screens/main/MainScreen.dart
+++ app/lib/screens/main/MainScreen.dart
@@ -12,7 +12,7 @@ class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<AuthManager>(
-      create: (_) => AuthManager(),
+      create : (_) => AuthManager(),
       child: Scaffold(),
     );
   }
